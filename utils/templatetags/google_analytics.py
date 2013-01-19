7000d3010296a05887d44b63e9d374ea5e05d363
Jesse the Game <jtg@localhost.(none)>
2013-01-19 05:10:27 +0000

    Add some comments to google_analytics template tag

--- utils/templatetags/google_analytics.py
+++ utils/templatetags/google_analytics.py
@@ -1,9 +1,18 @@
 from django.conf import settings
 from django import template
+
 register = template.Library()
 
 @register.simple_tag
 def google_analytics():
+    """
+    Requires two settings:
+
+    GOOGLE_ANALYTICS (bool)
+    GOOGLE_SITE_ID (the site ID provided by google)
+
+    If the settings are not present, this template tag will fail loudly.
+    """
     if not settings.GOOGLE_ANALYTICS:
         return ''
     return (
