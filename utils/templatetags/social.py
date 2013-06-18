40037902484072b569c1e4f230b64680f54586fb
Jesse the Game <jtg@li499-237.members.linode.com>
2013-06-18 00:35:03 +0000

    Update social to use www in stead of callm

--- utils/templatetags/social.py
+++ utils/templatetags/social.py
@@ -1,4 +1,4 @@
-import callm
+import www
 
 from django import template
 
@@ -15,7 +15,7 @@ def twitter_share(context, url=None, text='', via=None):
         query['text'] = text
     if via:
         query['via'] = via
-    return callm.URL('https://twitter.com/intent/tweet', query=query, verbatim=True)
+    return www.URL('https://twitter.com/intent/tweet', query=query, verbatim=True)
 
 @register.simple_tag(takes_context=True)
 def facebook_share(context, url=None, title=None, summary=None, images=None):
@@ -33,7 +33,7 @@ def facebook_share(context, url=None, title=None, summary=None, images=None):
             images = [images]
         for idx, image in enumerate(images):
             query['p[images][%s]' % idx] = image
-    return callm.URL('http://www.facebook.com/sharer/sharer.php', query=query, verbatim=True)
+    return www.URL('http://www.facebook.com/sharer/sharer.php', query=query, verbatim=True)
 
 @register.simple_tag(takes_context=True)
 def googleplus_share(context, url=None):
@@ -42,7 +42,7 @@ def googleplus_share(context, url=None):
         query['url'] = url
     else:
         query['url'] = context['request'].build_absolute_url()
-    return callm.URL('https://plus.google.com/share', query=query)
+    return www.URL('https://plus.google.com/share', query=query)
 
 @register.simple_tag(takes_context=True)
 def pinterest_share(context, media, url=None, description=None):
@@ -53,7 +53,7 @@ def pinterest_share(context, media, url=None, description=None):
         query['url'] = context['request'].build_absolute_url()
     if description:
         query['description'] = description
-    return callm.URL('http://pinterest.com/pin/create/button/', query=query)
+    return www.URL('http://pinterest.com/pin/create/button/', query=query)
 
 
 @register.simple_tag(takes_context=True)
@@ -65,4 +65,6 @@ def tumblr_photo(context, source, url=None, caption=None):
         query['clickthru'] = context['request'].build_absolute_url()
     if caption:
         query['caption'] = caption
-    return callm.URL('http://www.tumblr.com/share/photo', query=query)
+    return www.URL('http://www.tumblr.com/share/photo', query=query)
+
+
